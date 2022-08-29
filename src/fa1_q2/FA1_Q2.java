ae0b60711b1456ff09bba927fbfa3dba3e39bfc2
Roelof Kristensen <89071802+RoelofKristensen@users.noreply.github.com>
2022-08-29 08:51:49 +0200

    Push new project

---
+++ src/fa1_q2/FA1_Q2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fa1_q2;
+
+/**
+ *
+ * @author rjnkr
+ */
+public class FA1_Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FA1_Q2_JFrame question2 = new FA1_Q2_JFrame();
+        question2.setVisible(true);
+    }
+    
+}
